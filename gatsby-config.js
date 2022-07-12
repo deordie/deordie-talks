43d5fef62f507fe579540c0f485fca0c505bfb37
asm0dey <pavel.finkelshtein@gmail.com>
2022-07-12 15:05:31 +0300

    Updates everything

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,8 +123,26 @@ module.exports = {
         ],
       },
     },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+  
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
